171c792eb1f28813e9fe677c15bb525f4deae7f3
Nathan Kidd <nathankidd@hey.com>
2026-02-23 17:42:42 +0000

    windmill-labs/windmill: init at 4.0.84

---
+++ charts/windmill-labs/windmill/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://windmill-labs.github.io/windmill-helm-charts/";
+  chart = "windmill";
+  version = "4.0.84";
+  chartHash = "sha256-r9meK6MNjHIgRXpfOkfLwhurwhOidCj84x4d5XpCJIw=";
+}

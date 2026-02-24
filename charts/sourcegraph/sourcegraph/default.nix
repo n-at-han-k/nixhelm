17d7a2662aba513dfac2ed37946809a50e47fb74
Nathan Kidd <nathankidd@hey.com>
2026-02-24 02:51:56 +0000

    Quick Git Push

--- charts/sourcegraph/sourcegraph/default.nix
+++ charts/sourcegraph/sourcegraph/default.nix
@@ -1,6 +1,6 @@
 {
-  repo = "https://helm.sourcegraph.com/release";
+  repo = "oci://oci.trueforge.org/truecharts";
   chart = "sourcegraph";
-  version = "6.12.5040";
-  chartHash = "sha256-sICweQICSii2ogSLUl5eiyItCbi35nalRbiQhgmUy+8=";
+  version = "9.22.0";
+  chartHash = "sha256-vwudsaql2kJ6uAn873MDcV72clq3wm6GEttp1xasG+4=";
 }

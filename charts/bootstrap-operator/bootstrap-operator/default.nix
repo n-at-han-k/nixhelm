b651e29d695301f8632bfbf028d82fdb9f960ad2
Nathan Kidd <nathankidd@hey.com>
2026-03-05 06:50:41 +0000

    bootstrap-operator/bootstrap-operator: init at 0.0.0

---
+++ charts/bootstrap-operator/bootstrap-operator/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://n-at-han-k.github.io/bootstrap-operator";
+  chart = "bootstrap-operator";
+  version = "0.0.0";
+  chartHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+}
